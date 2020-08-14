84bb8244b6206c65e56b4d0e4e91c0654bebb7ab
Jungmin Hong <jungmin011107@gmail.com>
2020-08-14 21:16:22 +0900

    time,post

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    content = models.TextField
+    content = models.TextField()
 
-    created = models.DateTimeField
+    created = models.DateTimeField()
 
     author = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} :: {}'.format(self.title,self.author)
